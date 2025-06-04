198f9ce6db43961e21cf92997206f5169ebce355
G30TR1X <gavriel10donovan@gmail.com>
2025-06-04 18:31:54 +0700

    Added Boilerplate Code

--- DiskSchedulingAlgorithm.c
+++ DiskSchedulingAlgorithm.c
@@ -1,6 +1,123 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+enum algorithm {
+    FCFS = 1,
+    SCAN,
+    CSCAN,
+    LOOK,
+    CLOOK,
+    EXIT,
+};
+
+void copyArray(int *originalArray, int *newArray, int size);
+void firstComeFirstServe(int *device, int deviceAmount);
+void scan(int *device, int deviceAmount);
+void cScan(int *device, int deviceAmount);
+void cScan(int *device, int deviceAmount);
+void look(int *device, int deviceAmount);
+void cLook(int *device, int deviceAmount);
 
 int main()
 {
+    int deviceAmount;
+    printf("Amount of Devices: ");
+    scanf(" %d", &deviceAmount);
+    printf("\n");
+
+    int device[deviceAmount];
+    for (int i = 0; i < deviceAmount; i++)
+    {
+        printf("Device %d: ", i+1);
+        scanf(" %d", &device[i]);
+
+        if (device[i] > 199 || device[i] < 0)
+            i--, printf("Please input a number from 0 - 199\n\n");
+    }
+    printf("\n");
+
+    int deviceCopy[deviceAmount];
+
+    int choice;
+    while (choice != EXIT)
+    {
+        printf("Pick your Disk Scheduling Algorithm!\n");
+        printf("%d First Come First Serve\n", FCFS);
+        printf("%d SCAN\n", SCAN);
+        printf("%d C-SCAN\n", CSCAN);
+        printf("%d LOOK\n", LOOK);
+        printf("%d C-LOOK\n", CLOOK);
+        printf("%d Exit\n", EXIT);
+        printf("Choice: ");
+        scanf(" %d", &choice);
+
+        copyArray(device, deviceCopy, deviceAmount);
+
+        switch (choice)
+        {
+            case FCFS:
+                firstComeFirstServe(device, deviceAmount);
+                break;
+            case SCAN:
+                scan(device, deviceAmount);
+                break;
+            case CSCAN:
+                cScan(device, deviceAmount);
+                break;
+            case LOOK:
+                look(device, deviceAmount);
+                break;
+            case CLOOK:
+                cLook(device, deviceAmount);
+                break;
+            case EXIT:
+                break;
+            default:
+                printf("Pick a number from %d - %d", FCFS, EXIT);
+        }
+    }
+
     return 0;
 }
+
+void copyArray(int *originalArray, int *newArray, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = originalArray[i];
+    }
+
+    return ;
+}
+
+void firstComeFirstServe(int *device, int deviceAmount)
+{
+
+    return ;
+}
+
+void scan(int *device, int deviceAmount)
+{
+
+    return ;
+}
+
+void cScan(int *device, int deviceAmount)
+{
+
+    return ;
+}
+
+void look(int *device, int deviceAmount)
+{
+
+    return ;
+}
+
+void cLook(int *device, int deviceAmount)
+{
+
+    return ;
+}
+
--- PageReplacementAlgorithm.c
+++ PageReplacementAlgorithm.c
@@ -1,6 +1,112 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+enum algorithm {
+    FIFO = 1,
+    SECONDCHANCE,
+    LRU,
+    OPTIMAL,
+    EXIT,
+};
+
+void copyArray(int *originalArray, int *newArray, int size);
+void cleanFrame(int *frame, int frameSize);
+void firstInFirstOut(int *frame, int frameSize, int *reference, int referenceAmount);
+void secondChance(int *frame, int frameSize, int *reference, int referenceAmount);
+void leastRecentlyUsed(int *frame, int frameSize, int *reference, int referenceAmount);
+void optimalPageReplacement(int *frame, int frameSize, int *reference, int referenceAmount);
 
 int main()
 {
+    int frameSize;
+    int referenceAmount;
+    printf("Frame Size: ");
+    scanf(" %d", &frameSize);
+    printf("Amount of References: ");
+    scanf(" %d", &referenceAmount);
+    printf("\n");
+
+    int frame[frameSize];
+    int referenceInput[referenceAmount];
+    for (int i = 0; i < referenceAmount; i++)
+    {
+        printf("Reference %d: ", i+1);
+        scanf(" %d", &frame[i]);
+    }
+    printf("\n");
+
+    int referenceCopy[referenceAmount];
+
+    int choice;
+    while (choice != EXIT)
+    {
+        printf("Pick your Replacement Algorithm!\n");
+        printf("%d First In First Out\n", FIFO);
+        printf("%d Second Chance\n", SECONDCHANCE);
+        printf("%d Least Recently Used\n", LRU);
+        printf("%d Optimal Page Replacement\n", OPTIMAL);
+        printf("%d Exit\n", EXIT);
+        printf("Choice: ");
+        scanf(" %d", &choice);
+
+        cleanFrame(frame, frameSize);
+        copyArray(referenceInput, referenceCopy, referenceAmount);
+
+        switch (choice)
+        {
+            case FIFO:
+                firstInFirstOut(frame, frameSize, referenceCopy, referenceAmount);
+                break;
+            case EXIT:
+                break;
+            default:
+                printf("Pick a number from %d - %d", FIFO, EXIT);
+        }
+    }
+
     return 0;
+}
+
+void copyArray(int *originalArray, int *newArray, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = originalArray[i];
+    }
+
+    return ;
+}
+
+void cleanFrame(int *frame, int frameSize)
+{
+    for (int i = 0; i < frameSize; i++)
+    {
+        frame[i] = INT_MIN;
+    }
+
+    return ;
+}
+
+void firstInFirstOut(int *frame, int frameSize, int *reference, int referenceAmount)
+{
+
+    return ;
+}
+
+void secondChance(int *frame, int frameSize, int *reference, int referenceAmount)
+{
+
+    return ;
+}
+
+void leastRecentlyUsed(int *frame, int frameSize, int *reference, int referenceAmount)
+{
+
+    return ;
+}
+void optimalPageReplacement(int *frame, int frameSize, int *reference, int referenceAmount)
+{
+
+    return ;
 }
